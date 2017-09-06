b9cfbabbe230b7f847a92d1cc14b3bcd0813d202
the1337chef <the1337chef@ufl.edu>
2017-09-06 17:28:08 -0400

    Completed Assignment 3

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,15 +7,34 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config.js');
 
 /* Connect to your database */
-
+mongoose.connect('mongodb://cmndrbacara:twipapke@ds133398.mlab.com:33398/the1337chef-db');
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
-
+fs.readFile('listings.json', 'utf8', function(err, data)
+{
+	var newData = JSON.parse(data);
+	for(var i = 0; i < newData.entries.length; i++)
+	{
+		Listing.create(
+		{
+			code: newData.entries[i].code,
+			name: newData.entries[i].name,
+			coordinates: newData.entries[i].coordinates,
+			address: newData.entries[i].address,
+			created_at: newData.entries[i].created_at,
+			updated_at: newData.entries[i].updated_at
+		}, function(err, listing)
+		{
+			if(err)
+				return handleError(err);
+		});
+	}
+});
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- queries.js
+++ queries.js
@@ -1,28 +1,72 @@
 /* Fill out these functions using Mongoose queries*/
 
-var findLibraryWest = function() {
+var findLibraryWest = function()
+{
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+   Listing.find({ name: 'Library West'}, function(err, listing)
+	   {
+		   if(err)
+			   throw err;
+		   
+		   console.log(listing);
+	   });
 };
-var removeCable = function() {
+var removeCable = function()
+{
   /*
     Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+   Listing.find({ code: 'CABL'}, function(err, listing)
+	   {
+		   if(err)
+			   throw err;
+		   
+		   listing.remove(function(err)
+		   {
+				if(err) throw err;
+				
+				console.log(listing);
+		   });
+	   });
 };
-var updatePhelpsMemorial = function() {
+var updatePhelpsMemorial = function()
+{
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+   Listing.find({ name: 'Phelps Laboratory'}, function(err, listing)
+	   {
+		   if(err)
+			   throw err;
+		   
+		   listing.address = '701 N Broadway, Sleepy Hollow, NY 10591, United States';
+		   listing.save(function(err)
+		   {
+			   if(err)
+				   throw err;
+		   
+		   console.log(listing);
+		   });
+	   });
 };
-var retrieveAllListings = function() {
+var retrieveAllListings = function()
+{
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+   Listing.find({}, function(err, listings)
+	   {
+		   if(err)
+			   throw err;
+		   
+		   console.log(listings);
+	   });
 };
 
 findLibraryWest();
